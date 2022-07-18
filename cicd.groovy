3c068a2f65b2449feeb18641ce88aee7ef356550
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-18 08:47:07 -0400

    Add description to cicd.groovy script

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux') 
 {
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'topport'), string(name: 'DESCRIPTION', 'topport' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'topport'), string(name: 'DESCRIPTION', 'top is a task manager program, found in many Unix-like operating systems, that displays information about CPU and memory utilization.' )]
         }
 }
