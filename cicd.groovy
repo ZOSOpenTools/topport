2bd5652e74ea987c01557999f4cd421ed597a651
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-19 21:21:40 -0400

    Remove polling of utils.git

--- cicd.groovy
+++ cicd.groovy
@@ -10,6 +10,6 @@ node('linux')
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'topport'), string(name: 'DESCRIPTION', 'top is a task manager program, found in many Unix-like operating systems, that displays information about CPU and memory utilization.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'topport'), string(name: 'DESCRIPTION', value: 'top is a task manager program, found in many Unix-like operating systems, that displays information about CPU and memory utilization.' )]
         }
 }
