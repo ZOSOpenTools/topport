ef684e8aee98107d9a5f0180999c2e4eaa969439
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-20 10:00:57 -0400

    Add full repo url

--- cicd.groovy
+++ cicd.groovy
@@ -10,6 +10,6 @@ node('linux')
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'topport'), string(name: 'PORT_DESCRIPTION', value: 'top is a task manager program, found in many Unix-like operating systems, that displays information about CPU and memory utilization.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/topport.git'), string(name: 'PORT_DESCRIPTION', value: 'top is a task manager program, found in many Unix-like operating systems, that displays information about CPU and memory utilization.' )]
         }
 }
